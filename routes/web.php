e89df9bd23123194fc513fcd3f6fe1f422992096
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2017-06-01 15:39:48 -0400

    Adding user authentication

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,34 @@
 |
 */
 
+// Public Routes
+
+Route::get('/', 'PostsController@index');
+
 Route::name('posts_path')->get('/posts', 'PostsController@index');
-
-Route::name('create_post_path')->get('posts/create', 'PostsController@create');
-
-Route::name('store_post_path')->post('posts', 'PostsController@store');
 
 Route::name('post_path')->get('/posts/{post}', 'PostsController@show');
 
-Route::name('edit_post_path')->get('posts/{post}/edit/', 'PostsController@edit');
 
-Route::name('update_post_path')->put('posts/{post}', 'PostsController@update');
+// Authenticated Routes
 
-Route::name('delete_post_path')->delete('posts/{post}', 'PostsController@delete');
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::name('create_post_path')->get('posts/create', 'PostsController@create');
+
+    Route::name('store_post_path')->post('posts', 'PostsController@store');
+
+    Route::name('edit_post_path')->get('posts/{post}/edit/', 'PostsController@edit');
+
+    Route::name('update_post_path')->put('posts/{post}', 'PostsController@update');
+
+    Route::name('delete_post_path')->delete('posts/{post}', 'PostsController@delete');
+
+});
+
+Auth::routes();
+
+
+
